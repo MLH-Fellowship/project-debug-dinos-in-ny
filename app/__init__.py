52363f8a1abeb48eb2f84db1a0c985fa6273d3ff
Sristi Panchu <sristipanchu@gmail.com>
2022-06-06 16:49:33 -0400

    flask imp

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,29 @@ import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 
+class Person:
+    def __init__(self, f_name, l_name, school):
+        self.f_name = f_name
+        self.l_name = l_name
+        self.school = school
+
+
+
 load_dotenv()
 app = Flask(__name__)
 
 
+sristi = Person("Sristi", "Panchu", "Tufts University")
+aima = Person("Aima", "Alakhume", "New York University")
+
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html', title="Our Portfolio", url=os.getenv("URL"))
+
+@app.route('/aima')
+def abaima():
+    return render_template('aboutaima.html', student = aima)
+
+@app.route('/sristi')
+def absristi():
+    return render_template('aboutsristi.html', student = sristi)
