34d28b1ae7d294a3c9259d694cf72d347a85a5b3
Aima Alakhume <71132341+AimaAlakhume@users.noreply.github.com>
2022-06-27 00:58:20 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from peewee import *
+from datetime import *
+from playhouse.shortcuts import model_to_dict
 
 class Person:
     def __init__(self, f_name, l_name, school, major, hob1, hob2, hob3, hob4):
@@ -18,6 +21,15 @@ class Person:
 load_dotenv()
 app = Flask(__name__)
 
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    port=3306
+    )
+
+print(mydb)
+
 
 sristi = Person("Sristi", "Panchu", "Tufts University", "Computer Science", "crafting (specifically quilling)", "dancing", "reading", "exploring new places")
 aima = Person("Aima", "Alakhume", "New York University", "Electrical Engineering", "painting", "writing", "reading", "exploring new places")
@@ -33,3 +45,46 @@ def abaima():
 @app.route('/sristi')
 def absristi():
     return render_template('aboutsristi.html', student = sristi)
+
+
+
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.now)
+
+    class Meta:
+        database = mydb
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
+
+
+#POST route
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+
+    return model_to_dict(timeline_post)
+
+
+#GET endpoint
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline_posts': [
+            model_to_dict(p)
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
+
+
+#timeline page
+@app.route('/timeline')
+def timeline():
+    return render_template('timeline.html', title='Timeline')
+
