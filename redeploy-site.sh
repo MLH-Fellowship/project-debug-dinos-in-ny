6cc63bcf27b9ff344486d708d6af349c02ef0413
Aima Alakhume <71132341+AimaAlakhume@users.noreply.github.com>
2022-06-27 02:50:14 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 tmux kill-server
@@ -9,7 +8,6 @@ git fetch && git reset origin/main --hard
 python3 -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-pip install --upgrade pip
-pip install -r requirements.txt
 
-tmux new-session -d -s rbt123
+tmux new-session -d -s 
+systemctl restart myporfolio.service
