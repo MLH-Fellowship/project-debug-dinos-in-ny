f698b5e573ebc090767ba872e0ee853b0b720308
Sristi <69159569+sristii@users.noreply.github.com>
2022-06-27 14:50:17 -0400

    Create curl-test.sh

---
+++ curl-test.sh
@@ -0,0 +1,5 @@
+curl --request GET http://localhost:5000/api/timeline_post 
+curl --request POST http://localhost:5000/api/timeline_post -d 'name=Sristi&email=sristipanchu@gmail.com&con tent=JustAdded Database to my portfolio site!'
+curl --X POST http://localhost:5000/api/timeline_post 'name=Sristi&email=sristipanchu@gmail.com&con tent=JustAdded Database to my portfolio site!' -d
+
+curl --request http://localhost:5000/api/timeline_post 
