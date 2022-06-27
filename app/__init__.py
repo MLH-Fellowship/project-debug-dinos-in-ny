0513c05b965bda6372431923bedc73d76144d28d
sristii <sristipanchu@gmail.com>
2022-06-27 05:28:39 +0000

    mydb

--- app/__init__.py
+++ app/__init__.py
@@ -19,13 +19,13 @@ class Person:
 load_dotenv()
 app = Flask(__name__)
 
-mydb = 
-MySQLDatabase(os.getenv( "MYSQL_DATABASE"),
+mydb = MySQLDatabase(os.getenv( "MYSQL_DATABASE"),
     user=os.getenv("MYSQL_USER"),
     password=os.getenv( "MYSQL_PASSWORD"),
     host=os.getenv("MYSQL_HOST"),
     port=3306
 )
+
 print (mydb)
 
 sristi = Person("Sristi", "Panchu", "Tufts University", "Computer Science", "crafting (specifically quilling)", "dancing", "reading", "exploring new places")
