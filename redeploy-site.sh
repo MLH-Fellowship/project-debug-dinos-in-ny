414b56f33c72477a4365ee2b5cf4e21ce3710641
Sristi <69159569+sristii@users.noreply.github.com>
2022-06-30 23:24:12 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -2,8 +2,6 @@
 
 cd
 
-tmux kill-server
-
 cd project-debug-dinos-in-ny
 
 git fetch && git reset origin/main --hard
@@ -12,6 +10,5 @@ python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-tmux new -d -s site
-
-tmux send -t site "flask run --host=0.0.0.0" Enter
+systemctl daemon-reload
+systemctl restart myportfolio
