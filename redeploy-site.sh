0dacb312584ef0debe49c34b55449b25a50e840a
Aima Alakhume <71132341+AimaAlakhume@users.noreply.github.com>
2022-07-02 13:13:18 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-sudo apt-get install tmux
+cd /root/mlh-portfolio
 
-tmux kill-server
-cd mlh-portfolio
+git fetch && git reset --hard  origin/main
 
-git fetch && git reset origin/main --hard
-
-python3 -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-tmux new-session -d -s 
+systemctl restart myportfolio.service
