97c131da427aba058086c3e24e7afe7aea4e7a4b
Shivani Rajgopal <88949973+shivanircodel@users.noreply.github.com>
2022-07-03 21:13:03 -0400

    Update tests_app.py

--- tests/tests_app.py
+++ tests/tests_app.py
@@ -3,3 +3,14 @@ def test_malformed_timeline_post(self):
     assert response.status_code == 400
     html = response.get_data(as_text=True)
     assert "Invalid name" in html
+    
+   response= self.client.post("/api/timeline_post", data= {"name":"John Doe","email":"john@example.com","content":""})
+       assert response.status_code==400
+       html = response.get_data(as_text = True)
+       assert "Invalid content" in html 
+       
+    def test_malformed_timeline_post_email(self):
+       response = self.client.post("/api/timeline_post", data={"name":"aimailene@gmail.com","email":"not-an-email","content":"Hello world,I'm Aima!"})
+       assert response.status_code==400
+       html = response.get_data(as_text = True)
+       assert "Invalid email" in html 
