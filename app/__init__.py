610a7614b5c5be74ef50e933cc3a85c5c22e663e
Sristi Panchu <sristipanchu@gmail.com>
2022-07-09 21:32:36 -0400

    syntax

--- app/__init__.py
+++ app/__init__.py
@@ -14,13 +14,10 @@ class Person:
         self.hob3 = hob3
         self.hob4 = hob4
 
-
-
 load_dotenv()
 app = Flask(__name__)
 
-mydb = 
-MySQLDatabase(os.getenv( "MYSQL_DATABASE"),
+mydb = MySQLDatabase(os.getenv( "MYSQL_DATABASE"),
     user=os.getenv("MYSQL_USER"),
     password=os.getenv( "MYSQL_PASSWORD"),
     host=os.getenv("MYSQL_HOST"),
