42d1e71c11b4347786b2042cbbe94eb6ef7a4cde
Sristi <69159569+sristii@users.noreply.github.com>
2022-07-20 00:49:42 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-tmux kill-server
-
 git fetch && git reset origin/main --hard
 
-python -m venv python3-virtualenv
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-pip install --upgrade pip
-pip install peewee
-pip install pymysql
+docker compose -f docker-compose.prod.yml down
 
-tmux new -d -s site
-
-tmux send -t site "flask run --host=0.0.0.0" Enter
-
-#systemctl daemon-reload
-#systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml up -d --build
